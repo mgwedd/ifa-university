b5ea2b84b243ffb1361a59d5880bc2829e90f1d8
mgwedd <michael.wedd@leverege.com>
2019-05-21 15:15:40 -0400

    added core theme files and incorporated basic markup and styles for the site

---
+++ app/public/wp-content/themes/ifa-theme/functions.php
@@ -0,0 +1,10 @@
+<?php 
+// arg 1 tells wordpress to load some files. arg 2 is our function argument being passed in.
+function university_files() {
+    wp_enqueue_script('university_main_scripts', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Mono&display=swap');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
